35e0954540d1a24390fecb3012321f9a231ee908
johan <johan@baboons.se>
2019-12-05 00:09:17 +0100

    Fixes better handling for ENV

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -12,9 +12,12 @@ class TestCase extends \PHPUnit\Framework\TestCase
         if(is_file(__DIR__ . '/../.env')) {
             $dotenv = new Dotenv();
             $dotenv->load(__DIR__ . '/../.env');
+
+            putenv("VMS_API_KEY=" . $_ENV['VMS_API_KEY']);
+            putenv("VMS_ADMIN_API_KEY=" . $_ENV['VMS_ADMIN_API_KEY']);
         }
 
-        \Vms\Vms::setApiKey(getenv('VMS_API_KEY') || $_ENV['VMS_API_KEY']);
+        \Vms\Vms::setApiKey(getenv('VMS_API_KEY'));
         //\Vms\Vms::debug();
     }
 }
